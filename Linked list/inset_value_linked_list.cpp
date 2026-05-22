a8972f8d86fd5cce02f46a3b2899fabcbb9d7d02
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-05-22 23:54:33 +0600

    insert value

--- Linked list/inset_value_linked_list.cpp
+++ Linked list/inset_value_linked_list.cpp
@@ -1,23 +1,40 @@
 #include<iostream>
 using namespace std;
 
-class Node{
-    public:
-    Node* next;
+class Node {
+public:
     int data;
-    
-    Node(int data){
-        this->data= data;
-        this->next= NULL;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
     }
-    
 };
-void insert(int data){
+
+Node* head = NULL; // global head pointer
+
+void insertAtBegin(int data) {
     Node* newNode = new Node(data);
-    
+    newNode->next = head; // link new node to old head
+    head = newNode;       // update head
 }
 
-int main(){
-     
+void printList() {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main() {
+    insertAtBegin(10);
+    insertAtBegin(20);
+    insertAtBegin(30);
+
+    printList(); // Output: 30 -> 20 -> 10 -> NULL
+
     return 0;
 }
