45454cc51a5940fb5d17b298c1455b6737d58b3c
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-08-08 15:56:24 +0600

    add node count in binary tree

---
+++ Tree/node-count.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int val;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node* input_tree(){
+    int val;
+    cin>>val;
+    if(val==-1) return NULL;
+    Node* root = new Node(val);
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        Node* p = q.front();
+        q.pop();
+        int left_val, right_val;
+        cin>>left_val>>right_val;
+        if(left_val==-1){
+            p->left = NULL;
+        }
+        else{
+            p->left = new Node(left_val);
+            q.push(p->left);
+        }
+        if(right_val==-1){
+            p->right = NULL;
+        }
+        else{
+            p->right = new Node(right_val);
+            q.push(p->right);
+        }
+    
+    }
+    return root;
+
+}
+int count_nodes(Node* root){
+    if(root==NULL)
+    return 0;
+    int l = count_nodes(root->left);
+    int r = count_nodes(root->right);
+    return l + r + 1;
+}
+int main(){
+     Node* root = input_tree();
+     int node_count = count_nodes(root);
+     cout << node_count << endl;
+
+    return 0;
+}
