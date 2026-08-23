b221b4791cd7106f1f6de65b8f3e1831928f558d
Sanjida Aktar <sanjidabristy555@gmail.com>
2026-08-23 23:17:43 +0600

    add love of string

---
+++ Algorithm/module/adjacency matrix.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     int n,e;
+     cin>>n>>e;
+     int adj[n][n];
+     for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            adj[i][j]=0;
+        }
+     }
+     while (e)
+     {
+        /* code */
+     }
+     
+    return 0;
+}
